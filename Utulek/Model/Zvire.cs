b26531bf01691e86c78a085a68fd516ee47f54e6
stillhereD <dholoubkova42@gmail.com>
2025-11-03 08:16:14 +0100

    Vytvořeny promněné a konstruktor

--- Utulek/Model/Zvire.cs
+++ Utulek/Model/Zvire.cs
@@ -8,5 +8,25 @@ namespace Utulek.Model
 {
     internal class Zvire
     {
+        public int ID;
+        public string Jmeno;
+        public string Druh;
+        public int Vek;
+        public string Pohlavi;
+        public string DatumPrijmu; // dd/mm/yyyy př.10/9/2025
+        public string ZdravotniStav;
+        public string Poznamka;
+
+        public Zvire(string jmeno, string druh, int vek, string pohlavi, string datumprijmu, string zdravotnistav, string poznamka)
+        {
+            Jmeno = jmeno;
+            Druh = druh;
+            Vek = vek;
+            Pohlavi = pohlavi;
+            DatumPrijmu = datumprijmu;
+            ZdravotniStav = zdravotnistav;
+            Poznamka = poznamka;
+        }
+
     }
 }
