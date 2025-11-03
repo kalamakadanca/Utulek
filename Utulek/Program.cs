df7860274e18df689631f05d591fea438a17b5b1
BavorDDH <mates.bajer@gmail.com>
2025-11-03 08:33:05 +0100

    created method ZapisDoSouboru

--- Utulek/Program.cs
+++ Utulek/Program.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO; // pro operace se soubory
 using System.Threading.Tasks;
+using Utulek.Model;
 
 namespace Utulek
 {
+    
     internal class Program
     {
+        static void ZapisDoSouboru(string Soubor, Zvire Zvire)
+    {
+        string zvireparse = $"{Zvire.ID}@{Zvire.Jmeno}@{Zvire.Druh}@{Zvire.Vek}@{Zvire.Pohlavi}@{Zvire.DatumPrijmu}@{Zvire.ZdravotniStav}@{Zvire.Poznamka}";
+        using (StreamWriter sw = new StreamWriter(Soubor, true))
+        {
+            sw.WriteLine(zvireparse);
+        }
+    }
         static void Main(string[] args)
         {
         }
