d545132ce675ddf58b496b3a339c7c835775746f
BavorDDH <mates.bajer@gmail.com>
2025-11-21 13:15:27 +0100

    completed reading the txt file

--- Utulek/Program.cs
+++ Utulek/Program.cs
@@ -38,22 +38,47 @@ namespace Utulek
         {
             //TODO- I need to create an automatic generation of id based on the last id
             // TODO- I do not know if I should create an automatic filter based on this thing
-        string ProjectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
-        string FullPath = Path.Combine(ProjectPath, Soubor);
+            string ProjectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
+            string FullPath = Path.Combine(ProjectPath, Soubor);
             string ZvireTemp = $"{Zvire.ID}@{Zvire.Jmeno}@{Zvire.Druh}@{Zvire.Vek}@{Zvire.Pohlavi}@{Zvire.DatumPrijmu}@{Zvire.ZdravotniStav}@{Zvire.Poznamka}";
             // 1@Benny@pes@3@muz@2022-05-10@zdravy@pritelny a hravy - forma zapisu
-        using (StreamWriter sw = new StreamWriter(FullPath, true))
-        {
-            sw.WriteLine(ZvireTemp);
+            using (StreamWriter sw = new StreamWriter(FullPath, true))
+            {
+                sw.WriteLine(ZvireTemp);
+            }
         }
-    }
+        
+        static List<Zvire> VypisZvireZeSouboru(string Soubor)
+        {
+            string ProjectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
+            string FullPath = Path.Combine(ProjectPath, Soubor);
+            List<Zvire> Zvirata = new List<Zvire>();
+            using (StreamReader sr = new StreamReader(FullPath))
+            {
+                string Line;
+                while ((Line = sr.ReadLine()) != null)
+                {
+                    List<string> Parts = Line.Split('@').ToList();
+                    Zvire zvire = new Zvire(int.Parse(Parts[0]), Parts[1], Parts[2], int.Parse(Parts[3]), Parts[4], Parts[5], Parts[6], Parts[7]);
+                    Zvirata.Add(zvire);
+                }
+            }
+            return Zvirata;
+        }
+            
         static void Main(string[] args)
         {
-            Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "muz","2022/5/10", "zdravy", "pritelny a hravy");
-            Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "zena", "2023/1/15", "zdrava", "klidna a mazliva");
+            Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "male","2022/5/10", "zdravy", "pritelny a hravy");
+            Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "female", "2023/1/15", "zdrava", "klidna a mazliva");
 
             ZapisZvireDoSouboru("zvirata.txt", zvire1);
             ZapisZvireDoSouboru("zvirata.txt", zvire2);
+
+            List<Zvire> Zverina = VypisZvireZeSouboru("zvirata.txt");
+            foreach (var zvire in Zverina)
+            {
+                Console.WriteLine(zvire.Jmeno + zvire.ID + zvire.Vek);
+            }
         }
     }
 }
