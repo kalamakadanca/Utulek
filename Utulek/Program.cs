946b28156f11dc314c3962b1f18a99b5e72982ba
BavorDDH <mates.bajer@gmail.com>
2025-11-23 13:02:08 +0100

    pridany polozky adoptovano a datum adopce

--- Utulek/Program.cs
+++ Utulek/Program.cs
@@ -18,8 +18,10 @@ namespace Utulek
         public string DatumPrijmu { get; set; }
         public string ZdravotniStav { get; set; }
         public string Poznamka { get; set; }
+        public string Adoptovano { get; set; }
+        public string DatumAdopce { get; set; }
 
-        public Zvire(int id, string jmeno, string druh, int vek, string pohlavi, string datumPrijmu, string zdravotniStav, string poznamka)
+        public Zvire(int id, string jmeno, string druh, int vek, string pohlavi, string datumPrijmu, string zdravotniStav, string poznamka, string adoptovano,  string datumAdopce)
         {
             ID = id;
             Jmeno = jmeno;
@@ -29,6 +31,9 @@ namespace Utulek
             DatumPrijmu = datumPrijmu;
             ZdravotniStav = zdravotniStav;
             Poznamka = poznamka;
+            Adoptovano = adoptovano;
+            DatumAdopce = datumAdopce;
+            
         }
     }
     
@@ -37,17 +42,19 @@ namespace Utulek
         static void ZapisZvireDoSouboru(string Soubor, Zvire Zvire)
         {
             //TODO- I need to create an automatic generation of id based on the last id
-            // TODO- I do not know if I should create an automatic filter based on this thing
+            // TODO- I do not know if I should create a filter based on this thing
+            //TODO- I need to create a delete function based on id
+            //TODO- I need to create a filtered output reading based on species, age and name
             string ProjectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
             string FullPath = Path.Combine(ProjectPath, Soubor);
-            string ZvireTemp = $"{Zvire.ID}@{Zvire.Jmeno}@{Zvire.Druh}@{Zvire.Vek}@{Zvire.Pohlavi}@{Zvire.DatumPrijmu}@{Zvire.ZdravotniStav}@{Zvire.Poznamka}";
-            // 1@Benny@pes@3@muz@2022-05-10@zdravy@pritelny a hravy - forma zapisu
+            string ZvireTemp = $"{Zvire.ID}@{Zvire.Jmeno}@{Zvire.Druh}@{Zvire.Vek}@{Zvire.Pohlavi}@{Zvire.DatumPrijmu}@{Zvire.ZdravotniStav}@{Zvire.Poznamka}@ne@neadoptovano";
+            // 1@Benny@pes@3@muz@2022-05-10@zdravy@pritelny a hravy@ano/ne@2024-07-12 - forma zapisu
             using (StreamWriter sw = new StreamWriter(FullPath, true))
             {
                 sw.WriteLine(ZvireTemp);
             }
         }
-        
+
         static List<Zvire> VypisZvireZeSouboru(string Soubor)
         {
             string ProjectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
@@ -59,7 +66,7 @@ namespace Utulek
                 while ((Line = sr.ReadLine()) != null)
                 {
                     List<string> Parts = Line.Split('@').ToList();
-                    Zvire zvire = new Zvire(int.Parse(Parts[0]), Parts[1], Parts[2], int.Parse(Parts[3]), Parts[4], Parts[5], Parts[6], Parts[7]);
+                    Zvire zvire = new Zvire(int.Parse(Parts[0]), Parts[1], Parts[2], int.Parse(Parts[3]), Parts[4], Parts[5], Parts[6], Parts[7], Parts[8], Parts[9]);
                     Zvirata.Add(zvire);
                 }
             }
@@ -68,8 +75,8 @@ namespace Utulek
             
         static void Main(string[] args)
         {
-            Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "male","2022/5/10", "zdravy", "pritelny a hravy");
-            Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "female", "2023/1/15", "zdrava", "klidna a mazliva");
+            Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "male","2022/5/10", "zdravy", "pritelny a hravy", "ne", "neadoptovano");
+            Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "female", "2023/1/15", "zdrava", "klidna a mazliva", "Adoptovano", "2024/6/1");
 
             ZapisZvireDoSouboru("zvirata.txt", zvire1);
             ZapisZvireDoSouboru("zvirata.txt", zvire2);
