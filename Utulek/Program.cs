b943bb83a9be15efe3fb8b564d440a1a7d6a0615
BavorDDH <mates.bajer@gmail.com>
2025-11-23 18:00:46 +0100

    updated todo tree

--- Utulek/Program.cs
+++ Utulek/Program.cs
@@ -13,14 +13,15 @@ namespace Utulek
     {
         static void Main(string[] args)
         {
-            EvidenceUtulku evidence = new EvidenceUtulku();
-            Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "male", "2022/5/10", "zdravy", "pritelny a hravy", "ne", "neadoptovano");
-            Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "female", "2023/1/15", "zdrava", "klidna a mazliva", "ano", "2024/6/1");
+            //Zvire zvire1 = new Zvire(1, "Benny", "pes", 3, "male", "2022/5/10", "zdravy", "pritelny a hravy", "ne", "neadoptovano");
+            //Zvire zvire2 = new Zvire(2, "Micka", "kocka", 2, "female", "2023/1/15", "zdrava", "klidna a mazliva", "ano", "2024/6/1");
 
-            EvidenceUtulku.ZapisZvireDoSouboru("zvirata.txt", zvire1);
-            EvidenceUtulku.ZapisZvireDoSouboru("zvirata.txt", zvire2);
+            //EvidenceUtulku.ZapisZvireDoSouboru("zvirata.txt", zvire1);
+            //EvidenceUtulku.ZapisZvireDoSouboru("zvirata.txt", zvire2);
+            Zvire zvire3 = new Zvire("Rex", "pes", 4, "male", "2021/11/20", "zdravy", "ochranny a energicky", "ne", "neadoptovano");
+            EvidenceUtulku.ZapisZvireAutoID("zvirata.txt", zvire3);
 
-            List<Zvire> Zverina = EvidenceUtulku.VypisZvireZeSouboru("zvirata.txt");
+            List <Zvire> Zverina = EvidenceUtulku.VypisZvireZeSouboru("zvirata.txt");
             foreach (var zvire in Zverina)
             {
                 Console.WriteLine(zvire.Jmeno + zvire.ID + zvire.Vek);
